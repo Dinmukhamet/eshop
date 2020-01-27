f4efdd7c1cf4dad3352fac628960d1ae85ac1ba7
Dinmukhamet <igissinov_d@auca.kg>
2020-01-27 19:46:08 +0600

    Change settings. Update requirements

--- eshop/settings.py
+++ eshop/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+import dotenv
 import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'eshop-dimash.herokuapp.com']
 
 
 # Application definition
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        # 'HOST': config('DB_HOST'),
+        'HOST': config('DB_HOST'),
         'PORT': '',
     }
 }
