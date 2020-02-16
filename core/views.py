62873efd414d78238c5a12d87102204cbbbf48de
Dinmukhamet <igissinov_d@auca.kg>
2020-02-16 20:25:54 +0600

    Return back to the CSRF

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Max
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
 from .serializers import *
