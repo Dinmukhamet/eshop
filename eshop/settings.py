1f1408fe6daf6dfa6ac31ca0980a071b908bc7f8
Dinmukhamet <igissinov_d@auca.kg>
2020-03-22 18:03:24 +0600

    Fix typo

--- eshop/settings.py
+++ eshop/settings.py
@@ -150,7 +150,7 @@ JET_INDEX_DASHBOARD = 'dashboard.CustomIndexDashboard'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
