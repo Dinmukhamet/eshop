31554ce67f72e7423ff54e0b1a0bd95902c10760
Dinmukhamet <igissinov_d@auca.kg>
2020-03-22 17:33:05 +0600

    Work with static

--- eshop/settings.py
+++ eshop/settings.py
@@ -149,8 +149,8 @@ JET_INDEX_DASHBOARD = 'dashboard.CustomIndexDashboard'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/home/dinmukhamet/eshop/eshop/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
