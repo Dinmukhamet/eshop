54c4706a3f32f5c1d9bdc8fae6354facdac8b090
Dinmukhamet <igissinov_d@auca.kg>
2020-03-27 00:47:37 +0600

    Change dashboard

--- dashboard.py
+++ dashboard.py
@@ -8,35 +8,8 @@ class CustomIndexDashboard(Dashboard):
 
     def init_with_context(self, context):
         self.available_children.append(modules.LinkList)
-        self.children.append(modules.LinkList(
-            _('Support'),
-            children=[
-                {
-                    'title': _('Django documentation'),
-                    'url': 'http://docs.djangoproject.com/',
-                    'external': True,
-                },
-                {
-                    'title': _('Django "django-users" mailing list'),
-                    'url': 'http://groups.google.com/group/django-users',
-                    'external': True,
-                },
-                {
-                    'title': _('Django irc channel'),
-                    'url': 'irc://irc.freenode.net/django',
-                    'external': True,
-                },
-            ],
-            column=0,
-            order=0
-        ))
-
-class CoreIndexDashboard(Dashboard):
-    columns = 3
-
-    def init_with_context(self, context):
-        self.children.append(modules.ModelList(
-            _('Models'),
+        self.children.append(modules.AppList(
+            _('Applications'),
             exclude=('auth.*',),
             column=0,
             order=0
